8c7facca34330521c3f2c7b51804d93ad581abea
Vasilian05 <alexatanasov559@gmail.com>
2024-05-27 14:43:22 +0100

    file handling

--- classes/products.class.php
+++ classes/products.class.php
@@ -4,16 +4,79 @@ class Product extends Dbh {
 
    
     private $name;
-    private $img;
     private $category;
     private $description;
+    private $allowedExtensions = array('jpg', 'jpeg', 'png', 'webp');
+    private $maxFileSize = 500000; // 500KB
 
-    private function __construct($name, $img, $category, $description){
+    private function __construct($name, $category, $description, $allowedExtensions, $maxFileSize){
         $this->name=$name;
-        $this->img=$img;
         $this->category=$category;
         $this->description=$description;
+        $this->allowedExtensions=$allowedExtensions;
+        $this->maxFileSize=$maxFileSize;
+
     }
+
+    public function uploadImage($file) {
+        $errors = array();
+
+        // Validate file type
+        $fileExtension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        if (!in_array($fileExtension, $this->allowedExtensions)) {
+            $errors[] = "Само JPG, JPEG, PNG, и WEBP формати са позволени";
+        }
+
+        // Validate file size
+        if ($file['size'] > $this->maxFileSize) {
+            $errors[] = "Размерът на файла е твърде голям, максимумът е 500KB.";
+        }
+
+        // Check for errors
+        if (empty($errors)) {
+
+            if($this->category == "interior"){
+                $targetDir = "interior_img/";
+            }else{
+                $targetDir = "exterior_img/";
+            }
+            
+            $targetFile = $targetDir . basename($file['name']);
+            if (move_uploaded_file($file['tmp_name'], $targetFile)) {
+                return $targetFile; // Return the path to the uploaded file
+            } else {
+                $errors[] = "Sorry, there was an error uploading your file.";
+            }
+        }
+
+        return $errors; // Return an array of errors
+    }
+    //check if the door is interior or exterior
+    public function getCategory(){
+        if($this->category == "interior"){
+            //call method to import images in interior folder here
+        }else{
+            //call method to import images in exterior folder here
+        }
+
+
+    }
+
+    //get the image and put it in the folder
+    //import the image path to the database
+
+    //import the name of the door 
+    private function insertValues(){
+
+        $stmt = $this->connect()->prepare('INSERT INTO Doors(category_id, name, description) VALUES(?, ?, ?)');
+
+        if($stmt->execute($this->category, $this->name, $this->description)){
+            
+        }
+
+    }
+
+    //import the description
 
 
 
