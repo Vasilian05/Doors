3de1dc5066ebea9b8d77acf07609fc667c0dccdc
Vasilian05 <alexatanasov559@gmail.com>
2024-06-11 22:24:03 +0100

    user class

---
+++ classes/users.class.php
@@ -0,0 +1,14 @@
+<?php 
+include_once 'config.php';
+
+class User {
+
+    private $name;
+    private $company;
+    private $adress;
+    private $city;
+    private $phone;
+    private $email;
+    private $user_type;
+
+}
