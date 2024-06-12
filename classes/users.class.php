abcf7b928d0cb6e0524bed3a7adcbed735b66a1d
Vasilian05 <alexatanasov559@gmail.com>
2024-06-12 14:38:07 +0100

    validating names

--- classes/users.class.php
+++ classes/users.class.php
@@ -29,9 +29,34 @@ class User {
        
     }
 
-    private function checkEmty(){
-        if($this->name == "" || $this->company == "" || $this->adress == "" || $this->city == "" ){
-            //return
+    private function checkEmty(){// make sure fields are not empty
+        if($this->name == "" || $this->company == "" || $this->adress == "" || $this->city == "" || $this->phone == "" || $this->email = "" || $this->user_type == ""){
+            return false;
+        }else{
+            return true;
         }
     }
+
+
+    private function validateName(){
+        $error_message = "";
+        if(ctype_alpha($this->name)){// check for digits or symbols
+
+            if( strlen($this->name) >= 3 && strlen($this->name)< 24){   //length check
+                
+                return $error_message;
+            }else{
+                //if length is not valid
+                $error_message = "Името не може да е по-малко от 3 букви или повече от 24 букви";
+            }
+        }else{
+            //if name contains digits/symbols
+            $error_message = "Име не може да съдържа цифри или символи";
+        }
+        return $error_message;
+    }
+
+
+
+
 }
