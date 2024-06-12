d6e7cfcbf47fac5847702ee93245e49aef5d6263
Vasilian05 <alexatanasov559@gmail.com>
2024-06-12 12:58:38 +0100

    setters and started on checking if its empty

--- classes/users.class.php
+++ classes/users.class.php
@@ -9,6 +9,29 @@ class User {
     private $city;
     private $phone;
     private $email;
+    private $pass;
     private $user_type;
 
+    //Setters 
+    public function setRegisterDetails($name, $company, $adress, $city, $phone, $email, $user_type){
+        $this->name=$name;
+        $this->company=$company;
+        $this->adress=$adress;
+        $this->city=$city;
+        $this->phone=$phone;
+        $this->email=$email;
+        $this->user_type=$user_type;
+    }
+
+    public function setLogin($email, $pass){
+        $this->pass=$pass;
+        $this->email=$email;
+       
+    }
+
+    private function checkEmty(){
+        if($this->name == "" || $this->company == "" || $this->adress == "" || $this->city == "" ){
+            //return
+        }
+    }
 }
