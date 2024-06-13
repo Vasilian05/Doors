ebfa5ff736eb481e971b460937ce3255f6a40eb4
Vasilian05 <alexatanasov559@gmail.com>
2024-06-13 11:45:08 +0100

    front-end validation

--- register.php
+++ register.php
@@ -12,6 +12,7 @@ if(isset($_POST['submit'])){
     $phone = $_POST['phone'];
     $user_type = $_POST['user_type'];
 
+   
     $user = new User();
     $user->setRegisterDetails($name, $company, $city, $adress, $email, $phone, $user_type);
     if($user->AddUser() != ""){
@@ -26,23 +27,38 @@ if(isset($_POST['submit'])){
 <form method="POST" class="w-50 m-auto">
 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Име</label>
-    <input required type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input required type="text" name="name" class="form-control <?php echo $errors['name'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="invalid-feedback">
+          <?php echo $errors['name']?>
+    </div>
   </div>
 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Фирма</label>
-    <input required type="text" name="company" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input required type="text" name="company" class="form-control <?php echo $errors['company'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="invalid-feedback">
+          <?php echo $errors['company']?>
+    </div>
   </div>
 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Град</label>
-    <input required type="text" name="city" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input required type="text" name="city" class="form-control <?php echo $errors['city'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="invalid-feedback">
+          <?php echo $errors['city']?>
+    </div>
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Адрес</label>
-    <input required type="text" name="adress" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input required type="text" name="adress" class="form-control <?php echo $errors['adress'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="invalid-feedback">
+          <?php echo $errors['adress']?>
+    </div>
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Имейл</label>
-    <input required type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input required type="email" name="email" class="form-control <?php echo $errors['email'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div class="invalid-feedback">
+          <?php echo $errors['email']?>
+    </div>
   </div>
   <div>
     <label for="exampleInputEmail1" class="form-label">Телефон</label>
@@ -55,6 +71,14 @@ if(isset($_POST['submit'])){
             <option value="distributor">Дистрибутор</option>
             <option value="admin">Админ</option>
             </select>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Парола</label>
+        <input required type="email" name="email" class="form-control <?php //echo $errors['email'] != '' ?  'is-invalid' :  "" ?>" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small class="mb-3">Задължително само за админ акаунти</small><br><br>
+    <div class="invalid-feedback">
+          <?php echo $errors['email']?>
+    </div>
+  </div>
   <button type="submit" name="submit" class="btn btn-outline-dark">Създай акаунт</button>
 </form>
 
