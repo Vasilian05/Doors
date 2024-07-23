4039523fed39a48c9375e7faeeb4deeb53e1e164
Vasilian05 <alexatanasov559@gmail.com>
2024-07-23 13:12:48 +0300

    last door and alt text
    minor changes

--- product.php
+++ product.php
@@ -15,7 +15,7 @@ $door_data = $door->getDoor($door_id);
         <h1 class="mt-3"><?php echo $display_cat =  $category=="interior" ? "Интериорни": "Екстериорни";?> врати ArtDecorDoors</h1>
       <div class="row justify-content-evenly row-cols-1 row-cols-md-2 mt-5">
         <div class="col-lg-4 col-md-6 text-center">
-          <img src="<?php echo $door_data[0]['image']?>" class="img-fluid" alt="">
+          <img src="<?php echo $door_data[0]['image']?>" class="img-fluid" alt="Врати, Интериорни врати, външни врати, врати за баня, двулицеви врати, Врати София">
         </div>
         <div class="col-lg-8 col-md-6">
           <h2><?php echo $door_data[0]['name'] ?></h2>
