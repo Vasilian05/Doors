b36a8376b48bb0f40471d3f7949059b373663ccd
Vasilian05 <alexatanasov559@gmail.com>
2025-02-26 21:29:34 +0000

    xml sitemap and products page meta descriptions

--- products.php
+++ products.php
@@ -1,5 +1,5 @@
-<?php include_once "includes/header.php"; ?>
 <?php include_once "classes/doors.class.php"; ?>
+
 
 <?php
 
@@ -7,15 +7,20 @@ $doors = new Doors();
 // Retrieve the selected category from the URL parameters
 $category = isset($_GET['category']) ? $_GET['category'] : '';
 //
+
 // Generate content based on the selected category
 if ($category === 'interior') {
     // Generate content for interior doors
+    $page_title = "Интериорни врати – Стил и Качество за Вашия Дом";
+    $meta_description = "Разгледайте богатата ни селекция от интериорни врати, проектирани за съвременни интериори. Намерете перфектното съчетание от модерен дизайн, функционалност и издръжливост за вашия дом.";
     echo "<h2>Интериорни Врати</h2>";
     $data = $doors->getProducts(1);
     // Place your code here to fetch and display interior door items from the database or any other data source
 } else if ($category === 'exterior') {
     // Generate content for exterior doors
-    echo "<h2>Exterior Doors</h2>";
+    $page_title = "Входни врати – Стил и Качество за Вашия Дом";
+    $meta_description = "Разгледайте богатата ни селекция от Входни врати, проектирани за съвременни интериори. Намерете перфектното съчетание от модерен дизайн, функционалност и издръжливост за вашия дом.";
+    echo "<h2>Входни Врати</h2>";
     $data = $doors->getProducts(2);
     // Place your code here to fetch and display exterior door items from the database or any other data source
 } else if ($category == "facing"){
@@ -23,7 +28,7 @@ if ($category === 'interior') {
     $data = $doors->getFacing(3);
     echo "<p>No category selected or invalid category.</p>";
 }
-
+include_once "includes/header.php";
 ?>
 <div class="content">
     <section class="products mt-5">
---
+++ sitemap.php
@@ -0,0 +1,331 @@
+<?php
+
+header('Content-Type: application/xml; charset=utf-8');
+
+// Start the XML document
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <?php
+   // Example: manually defined URLs (you can also fetch these from your database)
+   $urls = [
+       [
+           'loc' => 'https://artdecordoors.com/index.php',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'daily',
+           'priority' => '1.0'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/products.php?category=interior',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=23',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=25',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=26',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=27',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=28',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=29',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=30',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=31',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=32',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=33',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=34',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=35',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=36',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=37',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=38',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=39',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=40',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=41',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=42',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=43',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=44',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=45',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=46',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=47',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=48',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=49',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=50',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=51',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=52',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=53',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=54',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=55',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=56',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=57',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=58',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=59',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=60',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=61',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=62',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=63',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=64',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=65',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=66',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=67',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=68',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=69',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/product.php?category=interior&id=70',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ]
+       ,
+       [
+           'loc' => 'https://artdecordoors.com/distributors.php',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ],
+       [
+           'loc' => 'https://artdecordoors.com/contacts.php',
+           'lastmod' => '2025-02-26',
+           'changefreq' => 'weekly',
+           'priority' => '0.8'
+       ]
+       // Add more URLs here
+   ];
+   
+   foreach ($urls as $url) {
+       echo '<url>';
+       echo '<loc>' . htmlspecialchars($url['loc']) . '</loc>';
+       echo '<lastmod>' . $url['lastmod'] . '</lastmod>';
+       echo '<changefreq>' . $url['changefreq'] . '</changefreq>';
+       echo '<priority>' . $url['priority'] . '</priority>';
+       echo '</url>';
+   }
+   ?>
+</urlset>
